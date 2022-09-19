ae623fba4a10d833912d5d846361148af2363a08
Giordano <giordano.mendes94@gmail.com>
2022-09-19 17:16:28 -0300

    requisito 3 - testes componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('testa página FavoritesPokemon ', () => {
+  test('verifica se se é exibida na tela a mensagem "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavoriteText = screen.getByText(/no favorite pokemon found/i);
+
+    expect(noFavoriteText).toBeInTheDocument();
+  });
+});
